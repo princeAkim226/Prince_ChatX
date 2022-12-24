8f6d5d2efd3e37e58bd2bab7974e21460134a9ca
Prince Akim <siathomas13@gmail.com>
2022-12-24 11:32:16 +0000

    update the server url2

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from "./assets/bot.svg";
-import user from "./assets/logo.PNG";
+import user from "./assets/logo.png";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
