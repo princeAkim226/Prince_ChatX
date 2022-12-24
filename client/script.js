7d8208e45ff63e2f6dba64e0eb800aa8839b7cd2
Prince Akim <siathomas13@gmail.com>
2022-12-24 11:34:40 +0000

    update the logo

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from "./assets/bot.svg";
-import user from "./assets/logo.png";
+import user from "./assets/logo.svg";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
